61301d7473afaa5f657b9c2002e9d5383a4ba789
Danilo Lustosa <danilolustosa@gmail.com>
2020-07-01 02:08:03 -0300

    First commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
+import './styles.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img className="logo" src={logo} alt="" />
+                </header>
+                <main>
+                    <h1>R&S em TI</h1>
+                    <p>Ajudando recrutadores a encontrarem os melhores candidatos</p>
+
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn />
+                        </span>
+                        <strong>Recrutar candidatos</strong>
+                    </Link>
+                </main>                
+            </div>
+        </div>
+    );
+}
+
+export default Home;
